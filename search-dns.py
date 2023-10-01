b9a702e1e554ee479ba824fe0d9cc4c3018fd6ef
patrick-projects <132287353+patrick-projects@users.noreply.github.com>
2023-10-01 10:53:55 -0400

    Add files via upload

---
+++ search-dns.py
@@ -0,0 +1,47 @@
+from concurrent.futures import ThreadPoolExecutor
+import dns.resolver
+import argparse
+import threading
+
+# Initialize a counter and a lock object
+counter = 0
+lock = threading.Lock()
+
+def check_spf(domain):
+    global counter
+
+    try:
+        answers = dns.resolver.resolve(domain, 'TXT')
+        for rdata in answers:
+            for txt_string in rdata.strings:
+                try:
+                    decoded_txt = txt_string.decode('utf-8')
+                except UnicodeDecodeError:
+                    decoded_txt = txt_string.decode('utf-8', 'ignore')
+
+                if "relay.mailchannels.net" in decoded_txt:
+                    print(f"{domain} has an SPF record containing relay.mailchannels.net.")
+    except (dns.resolver.NoAnswer, dns.resolver.NXDOMAIN):
+        pass
+
+    # Safely update counter
+    # Safely update counter
+    with lock:
+        counter += 1
+        if counter % 1000 == 0:
+            print(f"Processed {counter} out of {len(domains)} domains.")
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Check SPF records for domains from a file.')
+    parser.add_argument('filename', help='Path to the file containing the list of domains, one per line.')
+    args = parser.parse_args()
+
+    try:
+        with open(args.filename, 'r') as f:
+            domains = f.read().splitlines()
+    except FileNotFoundError:
+        print("File not found. Please provide a valid file path.")
+        exit(1)
+
+    with ThreadPoolExecutor() as executor:
+        executor.map(check_spf, domains)
